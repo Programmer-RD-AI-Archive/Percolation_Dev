79ca803d367cbfcadf242870463e2ce9a79dcc4f
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-03-13 23:56:06 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from Percolation import *
+
+row1 = create_row(10)
+row2 = create_row(10)
+row3 = create_row(10)
+rows = [row1,row2,row3]
+tot = ""
+for i in rows:
+    tot += row1 + "\n"
+print(tot)
