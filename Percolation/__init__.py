08a8eeb30d186b3c73b6bfef95360208e1eb1fcc
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-03-25 19:08:42 +0530

    Update __init__.py

--- Percolation/__init__.py
+++ Percolation/__init__.py
@@ -1,8 +1,17 @@
 import sys
 import random
 import os
-from prettytable import PrettyTable
 import datetime
+
+try:
+    from prettytable import PrettyTable
+except:
+    os.system("pip install prettytable")
+else:
+    os.system("python3 -m pip install prettytable")
+finally:
+    os.system("pip3 install prettytable")
+
 
 from Percolation.helper_functions import *
 from Percolation.grid_maker import *
