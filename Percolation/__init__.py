982fb9a4e49173ca88ee3ad1694b59c545f98b0a
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-03-27 12:47:14 +0530

    Update __init__.py

--- Percolation/__init__.py
+++ Percolation/__init__.py
@@ -7,10 +7,13 @@ try:
     from prettytable import PrettyTable
 except:
     os.system("pip install prettytable")
+    from prettytable import PrettyTable
 else:
     os.system("python3 -m pip install prettytable")
+    from prettytable import PrettyTable
 finally:
     os.system("pip3 install prettytable")
+    from prettytable import PrettyTable
 
 
 from Percolation.helper_functions import *
